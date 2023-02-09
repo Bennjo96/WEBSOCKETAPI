7db900936b92eac6624c744ee1dcc8437343470d
fabian <fabi.dev@hexle.at>
2023-02-09 14:14:00 +0100

    Score

--- src/main/java/io/boxhit/logic/GameLogicHandler.java
+++ src/main/java/io/boxhit/logic/GameLogicHandler.java
@@ -6,11 +6,11 @@ import io.boxhit.logic.subject.Player;
 public class GameLogicHandler {
 
     /**
-     * Add Score to Player
+     * Add or Remove Score to/from Player
      * @param player the player
      * @param score the specific score
      */
-    private static void addScore(Player player, Score score) {
+    private static void modifyScore(Player player, Score score) {
         player.setScore(player.getScore() + score.getScore());
     }
 }
